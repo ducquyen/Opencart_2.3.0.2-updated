3a53f9e8ac94667639a9d1e5b3a256f00675ec89
condor2 <condor20_05@yahoo.it>
2019-01-29 15:41:02 +0200

    Added category pagination fix

--- upload/system/library/pagination.php
+++ upload/system/library/pagination.php
@@ -28,15 +28,15 @@ class Pagination {
 		$num_links = $this->num_links;
 		$num_pages = ceil($total / $limit);
 
-		$this->url = str_replace('%7Bpage%7D', '{page}', $this->url);
+		$first_page_href = str_replace(array('&amp;page={page}', '?page={page}', 'page={page}',), '', $this->url);
 
 		$output = '<ul class="pagination">';
 
 		if ($page > 1) {
-			$output .= '<li><a href="' . str_replace(array('&amp;page={page}', '&page={page}'), '', $this->url) . '">' . $this->text_first . '</a></li>';
+			$output .= '<li><a href="' . $first_page_href . '">' . $this->text_first . '</a></li>';
 			
 			if ($page - 1 === 1) {
-				$output .= '<li><a href="' . str_replace(array('&amp;page={page}', '&page={page}'), '', $this->url) . '">' . $this->text_prev . '</a></li>';
+				$output .= '<li><a href="' . $first_page_href . '">' . $this->text_prev . '</a></li>';
 			} else {
 				$output .= '<li><a href="' . str_replace('{page}', $page - 1, $this->url) . '">' . $this->text_prev . '</a></li>';
 			}
@@ -66,7 +66,7 @@ class Pagination {
 					$output .= '<li class="active"><span>' . $i . '</span></li>';
 				} else {
 					if ($i === 1) {
-					$output .= '<li><a href="' . str_replace(array('&amp;page={page}', '&page={page}'), '', $this->url) . '">' . $i . '</a></li>';
+						$output .= '<li><a href="' . $first_page_href . '">' . $i . '</a></li>';
 					} else {
 						$output .= '<li><a href="' . str_replace('{page}', $i, $this->url) . '">' . $i . '</a></li>';
 					}
