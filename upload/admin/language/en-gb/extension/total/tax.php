1cd420062753ba05d1ac8fa099ded21cab482e7b
condor2 <condor20_05@yahoo.it>
2019-02-03 12:26:10 +0200

    Extensions language fix

--- upload/admin/language/en-gb/extension/total/tax.php
+++ upload/admin/language/en-gb/extension/total/tax.php
@@ -3,6 +3,7 @@
 $_['heading_title']    = 'Taxes';
 
 // Text
+$_['text_extension']   = 'Extensions';
 $_['text_total']       = 'Order Totals';
 $_['text_success']     = 'Success: You have modified taxes total!';
 $_['text_edit']        = 'Edit Tax Total';
