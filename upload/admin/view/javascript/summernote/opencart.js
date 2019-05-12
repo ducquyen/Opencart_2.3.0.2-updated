8144290736f812a4eeb3ff366f8be4d1641229e9
condor2 <condor20_05@yahoo.it>
2019-05-12 10:58:56 +0300

    Added summernote image attribute and font size

--- upload/admin/view/javascript/summernote/opencart.js
+++ upload/admin/view/javascript/summernote/opencart.js
@@ -3,31 +3,52 @@ $(document).ready(function() {
 	$('.summernote').each(function() {
 		var element = this;
 		
+		if ($(this).attr('data-lang')) {
+			$('head').append('<script type="text/javascript" src="view/javascript/summernote/lang/summernote-' + $(this).attr('data-lang') + '.js"></script>');
+		}
+
 		$(element).summernote({
+			lang: $(this).attr('data-lang'),
 			disableDragAndDrop: true,
 			height: 300,
 			emptyPara: '',
+			codemirror: { // codemirror options
+				mode: 'text/html',
+				htmlMode: true,
+				lineNumbers: true,
+				theme: 'monokai'
+			},			
+			fontsize: ['8', '9', '10', '11', '12', '14', '16', '18', '20', '24', '30', '36', '48' , '64'],
 			toolbar: [
 				['style', ['style']],
 				['font', ['bold', 'underline', 'clear']],
 				['fontname', ['fontname']],
+				['fontsize', ['fontsize']],
 				['color', ['color']],
 				['para', ['ul', 'ol', 'paragraph']],
 				['table', ['table']],
 				['insert', ['link', 'image', 'video']],
 				['view', ['fullscreen', 'codeview', 'help']]
 			],
+			popover: {
+           		image: [
+					['custom', ['imageAttributes']],
+					['imagesize', ['imageSize100', 'imageSize50', 'imageSize25']],
+					['float', ['floatLeft', 'floatRight', 'floatNone']],
+					['remove', ['removeMedia']]
+				],
+			},			
 			buttons: {
     			image: function() {
 					var ui = $.summernote.ui;
-
+							
 					// create button
 					var button = ui.button({
 						contents: '<i class="note-icon-picture" />',
 						tooltip: $.summernote.lang[$.summernote.options.lang].image.image,
 						click: function () {
 							$('#modal-image').remove();
-						
+							
 							$.ajax({
 								url: 'index.php?route=common/filemanager&token=' + getURLVar('token'),
 								dataType: 'html',
@@ -61,5 +82,4 @@ $(document).ready(function() {
   			}
 		});
 	});
-	
 });
