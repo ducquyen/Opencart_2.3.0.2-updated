db45f8becc6fded4fc9066c15c96cfc8c831c0d6
xxvirusxx <condor20_05@yahoo.it>
2019-11-12 16:31:03 +0200

    Update manufacturer.php

--- upload/catalog/controller/product/manufacturer.php
+++ upload/catalog/controller/product/manufacturer.php
@@ -89,7 +89,7 @@ class ControllerProductManufacturer extends Controller {
 		}
 
 		if (isset($this->request->get['page'])) {
-			$page = $this->request->get['page'];
+			$page = (int)$this->request->get['page'];
 		} else {
 			$page = 1;
 		}
